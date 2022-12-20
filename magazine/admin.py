cfda80b8f10a8c22d33696456f1ab7a210f23adc
frirsta <frista-8@hotmail.com>
2022-12-20 17:41:55 +0000

    Fix AdminPost class search field

--- magazine/admin.py
+++ magazine/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Post
 
+
+class AdminPost(admin.ModelAdmin):
+    list_display = ['title', 'author', 'post_created']
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ['title', 'content']
+    ordering = ['post_created']
+
+
+admin.site.register(Post, AdminPost)
 admin.site.register(User, UserAdmin)
 
 # Register your models here.
