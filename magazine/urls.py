0e50c9c4a25fd430284b5a149df49f74b9706608
frirsta <frista-8@hotmail.com>
2022-12-20 18:32:34 +0000

    Add HomePageView class. Add home.html. Add path to urls.py

--- magazine/urls.py
+++ magazine/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . views import SignUpView
+from . views import SignUpView, HomePageView
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup')
+    path('', HomePageView.as_view(), name='home'),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
--- magazine/views.py
+++ magazine/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render, redirect
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from .forms import SignUpForm
 from django.urls import reverse_lazy
-from django.views.generic import ListView
+from .models import Post
+
+
+class HomePageView(ListView):
+    """
+    This class shows all posts, and adds the newest post on top of the page.
+    The posts are ordered by date in decreasing order.
+    """
+
+    template_name = 'magazine/home.html'
+    queryset = Post.objects.all()
+    paginate_by = 6
 
 
 class SignUpView(CreateView):
