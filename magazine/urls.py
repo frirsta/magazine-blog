32e65712f4685fb7cc096d7136801e2beb683c92
frirsta <frista-8@hotmail.com>
2022-12-24 11:55:52 +0000

    Add url to change password and password change done pages. Add alert to base.html. Add password_change.html and password_change_done.html. Add change password button to profile page

--- magazine/urls.py
+++ magazine/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from . views import SignUpView, HomePageView, loginUser, logoutUser, profile_page
+from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
@@ -7,4 +8,7 @@ urlpatterns = [
     path('login/', loginUser, name='login'),
     path('logout/', logoutUser, name='logout'),
     path('profile/', profile_page, name='profile'),
+    path('password_change/', PasswordChangeView.as_view(template_name='users/password_change.html', success_url=reverse_lazy('magazine:password_change_done')), name='password_change'),
+    path('password_change/done/', PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),name='password_change_done'),
+
 ]
