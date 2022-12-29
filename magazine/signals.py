1c0fc7e5732486b27b9199a645b71edc194b738e
frirsta <frista-8@hotmail.com>
2022-12-29 12:43:15 +0000

    Remove function in signals

--- magazine/signals.py
+++ magazine/signals.py
@@ -7,8 +7,3 @@ from .models import User, Profile
 def create_profile(sender, created, instance, **kwargs):
     if created:
         Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
