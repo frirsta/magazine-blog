c377a6b3852cc7250c59d0bb7560807aa030ee26
frirsta <frista-8@hotmail.com>
2023-01-06 15:15:59 +0000

    Add javascript file and setTimeout function that removes the alerts after three seconds

--- blog/settings.py
+++ blog/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
 
@@ -47,6 +48,15 @@ INSTALLED_APPS = [
     'cloudinary',
     'magazine',
 ]
+
+
+MESSAGES_TAGS = {
+    messages.DEBUG: 'alert-',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 
 MIDDLEWARE = [
